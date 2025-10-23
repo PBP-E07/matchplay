3afe1a69e8c91183eb81079e426030ff1df0e127
fathan0104 <fathan.alfahrezi@ui.ac.id>
2025-10-24 01:01:51 +0700

    Equipment

---
+++ equipment/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Equipment, Rental
+
+admin.site.register(Equipment)
+admin.site.register(Rental)
---
+++ equipment/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    quantity = models.PositiveIntegerField(default=0)
+    price_per_hour = models.DecimalField(max_digits=8, decimal_places=2)
+    available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Rental(models.Model):
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    renter_name = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # otomatis hitung total harga
+        duration_hours = (self.end_time - self.start_time).total_seconds() / 3600
+        self.total_cost = duration_hours * float(self.equipment.price_per_hour)
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"{self.renter_name} - {self.equipment.name}"
