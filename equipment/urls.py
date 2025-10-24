7f28e57ec5e60055cb9d090bf2615d09446fde21
fathan0104 <fathan.alfahrezi@ui.ac.id>
2025-10-24 12:37:02 +0700

    equippment

--- equipment/urls.py
+++ equipment/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.equipment_list, name='equipment_list'),
     path('<int:id>/', views.equipment_detail, name='equipment_detail'),
+    path('add/', views.add_equipment, name='add_equipment'),
 ]
