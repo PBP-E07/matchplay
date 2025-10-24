fa13cc901c32cde9b7855e92846ceaa344ce9391
Kritzeey <190005533+Kritzeey@users.noreply.github.com>
2025-10-24 16:21:26 +0700

    feat: add hero section

--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from fields.models import Field
+
 # Create your views here.
 def show_main(request):
-    return render(request, "main.html")
+    fields = Field.objects.all()
+
+    context = { "fields":  fields }
+
+    return render(request, "main.html", context)
