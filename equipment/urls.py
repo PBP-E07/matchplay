f13029f1e5fba579b3bfbd73d455a0fe7ca4730d
fathan0104 <fathan.alfahrezi@ui.ac.id>
2025-12-22 03:43:11 +0700

    test

--- equipment/urls.py
+++ equipment/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.urls import path
-from .views import equipment_list, add_equipment, equipment_json_detail, edit_equipment, delete_equipment, show_json, create_equipment_flutter
+from .views import equipment_list, add_equipment, equipment_json_detail, edit_equipment, delete_equipment, show_json, create_equipment_flutter, edit_equipment_flutter, delete_equipment_flutter
 from . import views
 from .views import show_json
 
@@ -16,5 +16,7 @@ urlpatterns = [
     path('create-flutter/', create_equipment_flutter, name='create_equipment_flutter'),
     path('book/', views.book_equipment, name='book_equipment'),
     path('<int:id>/availability/', views.check_availability, name='check_availability'),
+    path('edit-flutter/<int:id>/', edit_equipment_flutter, name='edit_equipment_flutter'),
+    path('delete-flutter/<int:id>/', delete_equipment_flutter, name='delete_equipment_flutter'),
 ]
 
